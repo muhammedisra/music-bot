7417cfaf56c3c0d8a39873d294efa85e71b6354e
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-11-03 15:58:08 +0530

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -14,11 +14,11 @@ module.exports = {
 
         const commands = client.commands.filter(x => x.showHelp !== false);
 
-        embed.setDescription('This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.');
+        embed.setDescription('This bot is coded by isra7766.\n DM isra7766#9601 for any suggestion.');
         embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 
         embed.setTimestamp();
-        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter('Music comes first - Made with heart by isra7766 ❤️', message.author.avatarURL({ dynamic: true }));
 
         message.channel.send({ embeds: [embed] });
     },
